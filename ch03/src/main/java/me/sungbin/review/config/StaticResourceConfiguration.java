6d54868a173defaa55947fdd82cb2990fa2e1c82
Robert.Yang <ysb05222@naver.com>
2021-09-25 00:30:14 +0900

    스프링 프로젝트 실습
    스프링 프로젝트 실습

---
+++ ch03/src/main/java/me/sungbin/review/config/StaticResourceConfiguration.java
@@ -0,0 +1,31 @@
+package me.sungbin.review.config;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import org.springframework.web.servlet.resource.PathResourceResolver;
+
+@Configuration
+public class StaticResourceConfiguration  implements WebMvcConfigurer {
+
+  private static final String[] CLASSPATH_RESOURCE_LOCATIONS = {
+	        "classpath:/META-INF/resources/", 
+	        "classpath:/resources/",
+	        "classpath:/resources/static/",
+	        "classpath:/static/", 
+	        "classpath:/public/" };
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+    	if (!registry.hasMappingForPattern("/**")) {
+
+	        registry.addResourceHandler("/**")
+	                .addResourceLocations("CLASSPATH_RESOURCE_LOCATIONS")
+	                .setCachePeriod(3600)
+	                .resourceChain(true)
+	                .addResolver(new PathResourceResolver());
+    	}
+    }
+}
